a8530c44c3fd20197da06e4e2e62e4abb6e014b3
William Douglass <w.douglass1127@gmail.com>
2024-11-17 21:51:47 -0500

    Refactor hand_tracking.py

---
+++ hand_tracking.py
@@ -0,0 +1,25 @@
+import cv2 as cv
+import mediapipe as mp
+
+class HandTracking:
+
+    def __init__(self):
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands()
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def get_hands(self,input_img):
+
+        # bgr -> rgb
+        img_rgb = cv.cvtColor(input_img, cv.COLOR_BGR2RGB)
+
+        # Find Landmarks
+        results = self.hands.process(img_rgb)
+
+        return results
+
+    def draw_landmarks(self, img, results):
+        for handLMS in results.multi_hand_landmarks:
+            self.mpDraw.draw_landmarks(img, handLMS, mp.solutions.hands.HAND_CONNECTIONS)
+        return img
+
